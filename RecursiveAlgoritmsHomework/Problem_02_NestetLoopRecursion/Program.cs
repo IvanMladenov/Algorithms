633f7d20f71fee82dd6ac774a5165f77412a4c5b
IvanMladenov <ismladenov@gmail.com>
2015-09-21 23:52:56 +0300

    Nested Loops to recursion solved

---
+++ RecursiveAlgoritmsHomework/Problem_02_NestetLoopRecursion/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem_02_NestetLoopRecursion
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] combination = new int[n];
+
+            GenerateCombinations(n, combination, 0);
+        }
+
+        private static void GenerateCombinations(int n, int[] combination, int index)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                combination[index] = i;
+                if (index + 1 != n)
+                {
+                    GenerateCombinations(n, combination, index + 1);
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(" ", combination));
+                }
+            }
+        }
+    }
+}
