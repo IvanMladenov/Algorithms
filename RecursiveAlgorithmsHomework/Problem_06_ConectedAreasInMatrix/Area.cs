b36e8e5089afaafc2eac8b62427eee22365245bc
IvanMladenov <ismladenov@gmail.com>
2015-09-24 12:31:00 +0300

    Problem_06_ConectedAreasInMatrix/Area.cs works fine for examples int the homework

---
+++ RecursiveAlgorithmsHomework/Problem_06_ConectedAreasInMatrix/Area.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem_06_ConectedAreasInMatrix
+{
+    using System;
+
+    public class Area:IComparable<Area>
+    {
+        public Area(int[] position)
+        {
+            this.Position = position;
+        }
+
+        public int[] Position { get; set; }
+
+        public int Size { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Area #1 at ({0}, {1}), size: {2}", this.Position[0], this.Position[1], this.Size);
+        }
+
+        public int CompareTo(Area other)
+        {
+            int compareSize = -1*this.Size.CompareTo(other.Size);
+
+            if (compareSize == 0)
+            {
+                int compareRows = this.Position[0].CompareTo(other.Position[0]);
+
+                if (compareRows == 0)
+                {
+                    return this.Position[1].CompareTo(other.Position[1]);
+                }
+                else
+                {
+                    return compareRows;
+                }
+            }
+            else
+            {
+                return compareSize;
+            }
+        }
+    }
+}
---
+++ RecursiveAlgorithmsHomework/Problem_06_ConectedAreasInMatrix/ProgramMain.cs
@@ -0,0 +1,101 @@
+﻿namespace Problem_06_ConectedAreasInMatrix
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class ProgramMain
+    {
+        private static readonly SortedSet<Area> foundedAreas = new SortedSet<Area>();
+
+        private static readonly char[,] matrix =
+            {
+                { ' ', ' ', ' ', '*', ' ', ' ', ' ', '*', ' ' },
+                { ' ', ' ', ' ', '*', ' ', ' ', ' ', '*', ' ' },
+                { ' ', ' ', ' ', '*', ' ', ' ', ' ', '*', ' ' },
+                { ' ', ' ', ' ', ' ', '*', ' ', '*', ' ', ' ' }
+            };
+
+        //private static readonly char[,] matrix =
+        //    {
+        //        { '*', ' ', ' ', '*', ' ', ' ', ' ', '*', ' ', ' ' },
+        //        { '*', ' ', ' ', '*', ' ', ' ', ' ', '*', ' ', ' ' },
+        //        { '*', ' ', ' ', '*', '*', '*', '*', '*', ' ', ' ' },
+        //        { '*', ' ', ' ', '*', ' ', ' ', ' ', '*', ' ', ' ' },
+        //        { '*', ' ', ' ', '*', ' ', ' ', ' ', '*', ' ', ' ' }
+        //    };
+
+        private static readonly int RowsNum = matrix.GetLength(0);
+
+        private static readonly int ColsNum = matrix.GetLength(1);
+
+        private static void Main()
+        {
+            int?[] traversableCell = FindTraversableCell();
+
+            while (traversableCell != null)
+            {
+                int row = traversableCell[0].Value;
+                int col = traversableCell[1].Value;
+                int[] startPosition = { row, col };
+                Area currentArea = new Area(startPosition);
+                FindConnectedCells(row, col, currentArea);
+                foundedAreas.Add(currentArea);
+
+                traversableCell = FindTraversableCell();
+            }
+
+            PrintResult();
+        }
+
+        private static void PrintResult()
+        {
+            Console.WriteLine("Total areas found: " + foundedAreas.Count);
+
+            foreach (var area in foundedAreas)
+            {
+                Console.WriteLine(area);
+            }
+        }
+
+        private static void FindConnectedCells(int row, int col, Area area)
+        {
+            if (row < 0 || col < 0 || row >= RowsNum || col >= ColsNum)
+            {
+                return;
+            }
+
+            if (matrix[row, col] == 'v' || matrix[row, col] == '*')
+            {
+                return;
+            }
+
+            matrix[row, col] = 'v';
+            area.Size++;
+
+            FindConnectedCells(row, col + 1, area);
+            FindConnectedCells(row + 1, col, area);
+            FindConnectedCells(row, col - 1, area);
+            FindConnectedCells(row - 1, col, area);
+        }
+
+        private static int?[] FindTraversableCell()
+        {
+            int?[] traversableCell = new int?[2];
+            for (int row = 0; row < RowsNum; row++)
+            {
+                for (int col = 0; col < ColsNum; col++)
+                {
+                    if (matrix[row, col] != 'v' && matrix[row, col] != '*')
+                    {
+                        traversableCell[0] = row;
+                        traversableCell[1] = col;
+                        return traversableCell;
+                    }
+                }
+            }
+
+            traversableCell = null;
+            return traversableCell;
+        }
+    }
+}
