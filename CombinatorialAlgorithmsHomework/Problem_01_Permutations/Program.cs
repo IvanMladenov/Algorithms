073b6dd8a768268c652a082dffd78adb47f9a672
IvanMladenov <ismladenov@gmail.com>
2015-09-29 10:51:10 +0300

    Problem_01_Permutations.csproj solved

---
+++ CombinatorialAlgorithmsHomework/Problem_01_Permutations/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Problem_01_Permutations
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static int permutationsCount;
+
+        private static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var numbersArray = Enumerable.Range(1, n).ToArray();
+            Permute(numbersArray);
+            Console.WriteLine("Total permutations: " + permutationsCount);
+        }
+
+        private static void Permute(int[] numbersArray, int startIndex = 0)
+        {
+            if (startIndex == numbersArray.Length - 1)
+            {
+                Console.WriteLine(string.Join(", ", numbersArray));
+                permutationsCount++;
+            }
+            else
+            {
+                for (int i = startIndex; i < numbersArray.Length; i++)
+                {
+                    if (startIndex != i)
+                    {
+                        Swap(ref numbersArray[startIndex], ref numbersArray[i]);
+                    }
+        
+                    Permute(numbersArray, startIndex + 1);
+                    if (startIndex != i)
+                    {
+                        Swap(ref numbersArray[i], ref numbersArray[startIndex]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(ref int startIndex, ref int i)
+        {
+            startIndex ^= i;
+            i ^= startIndex;
+            startIndex ^= i;
+        }
+    }
+}
