3738695bfe096f863901fefe295563de0dd0e109
IvanMladenov <ismladenov@gmail.com>
2015-10-03 23:21:12 +0300

    Problem_04_GenerateSubsetsOfStringArray/Program.cs solved

---
+++ CombinatorialAlgorithmsHomework/CombinationsIteratively/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace CombinationsIteratively
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            foreach (var combination in GenerateCombinations(n, m))
+            {
+                for (int i = 0; i < combination.Length; i++)
+                {
+                    Console.Write(combination[i] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static IEnumerable<int[]> GenerateCombinations(int combinationNumbersCount, int numbersCount)
+        {
+            int[] result = new int[combinationNumbersCount];
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+
+            while (stack.Count > 0)
+            {
+                int index = stack.Count - 1;
+                int value = stack.Pop();
+
+                while (value <= numbersCount)
+                {
+                    result[index++] = value++;
+                    stack.Push(value);
+                    if (index == combinationNumbersCount)
+                    {
+                        yield return result;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CombinatorialAlgorithmsHomework/Problem_04_GenerateSubsetsOfStringArray/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem_04_GenerateSubsetsOfStringArray
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] set = {"test", "rock", "fun", "hey"};
+            int k = 3;
+
+            GenerateSubsets(set, k, new int[k], 0, 0);
+        }
+
+        private static void GenerateSubsets(string[] set, int k, int[] positions, int position, int startIndex)
+        {
+            if (position >= k)
+            {
+                foreach (var pos in positions)
+                {
+                    Console.Write(set[pos] + " ");
+                }
+
+                Console.WriteLine();
+                return;
+            }
+
+            for (int i = startIndex; i < set.Length; i++)
+            {
+                positions[position] = i;
+                GenerateSubsets(set, k, positions, position+1, i+1);
+            }
+        }
+    }
+}
