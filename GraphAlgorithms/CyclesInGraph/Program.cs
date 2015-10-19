8e03d2af81b99199512ac94962fc3a9d1b1fc7d4
IvanMladenov <ismladenov@gmail.com>
2015-10-20 01:51:48 +0300

    CiclesInGraph not working for last example, but I think it is correct

---
+++ GraphAlgorithms/CyclesInGraph/Program.cs
@@ -0,0 +1,83 @@
+﻿
+namespace CyclesInGraph
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<char, List<char>> graph = new Dictionary<char, List<char>>();
+            FillTheGraph(graph);
+
+            SortedList<char, int> predecessors = new SortedList<char, int>();
+            FindPredecessors(graph, predecessors);
+
+            bool nodeRemoved = true;
+            while (nodeRemoved)
+            {
+                nodeRemoved = false;
+                foreach (var node in predecessors.Keys)
+                {
+                    if (predecessors[node] == 0)
+                    {
+                        foreach (var child in graph[node])
+                        {
+                            if (predecessors.ContainsKey(child))
+                            {
+                                predecessors[child]--;
+                            }                       
+                        }
+
+                        predecessors.Remove(node);
+                        nodeRemoved = true;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Acyclic: {0}", predecessors.Count == 0 ? "Yes" : "No");
+        }
+
+        private static void FillTheGraph(Dictionary<char, List<char>> graph)
+        {
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+
+                char node = line[0];
+                char edge = line[4];
+                if (!graph.ContainsKey(node))
+                {
+                    graph.Add(node, new List<char>());
+                }
+
+                graph[node].Add(edge);
+            }
+        }
+
+        private static void FindPredecessors(Dictionary<char, List<char>> graph, SortedList<char, int> predecessors)
+        {
+            foreach (var node in graph.Keys)
+            {
+                predecessors.Add(node, 0);
+            }
+
+            foreach (var node in graph.Keys)
+            {
+                foreach (var child in graph[node])
+                {
+                    if (predecessors.ContainsKey(child))
+                    {
+                        predecessors[child]++;
+                    }                  
+                }
+            }
+        }
+    }
+}
