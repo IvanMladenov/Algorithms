395ebb0aa8a175c7e5e0d9483d3a48ca153c5b53
IvanMladenov <ismladenov@gmail.com>
2015-10-28 13:01:50 +0200

    Add Advanced algorithms homework and solution for problem 1

---
+++ AdvancedGraphAlgorithms/ExtendCableNetwork/Edge.cs
@@ -0,0 +1,31 @@
+﻿namespace ExtendCableNetwork
+{
+    using System;
+
+    public class Edge : IComparable<Edge>
+    {
+        public Edge(int startNode, int endNode, int weight)
+        {
+            this.StartNode = startNode;
+            this.EndNode = endNode;
+            this.Weight = weight;
+        }
+
+        public int StartNode { get; set; }
+
+        public int EndNode { get; set; }
+
+        public int Weight { get; set; }
+
+        public int CompareTo(Edge other)
+        {
+            int comp = this.Weight.CompareTo(other.Weight);
+            return comp;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0} {1}) -> {2}", this.StartNode, this.EndNode, this.Weight);
+        }
+    }
+}
---
+++ AdvancedGraphAlgorithms/ExtendCableNetwork/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ExtendCableNetwork
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.ReadLine();
+
+            List<Edge> edges = new List<Edge>();
+            Dictionary<int, bool> conectedNodes = new Dictionary<int, bool>();
+            int budget = 0;
+
+            while (true)
+            {
+                string currentLine = Console.ReadLine();
+                if (currentLine.Contains("Budget"))
+                {
+                    string[] lastLine = currentLine.Split(' ');
+                    budget = int.Parse(lastLine[1]);
+                    break;
+                }
+
+                string[] splitet = currentLine.Split(' ');
+                var startNode = int.Parse(splitet[0]);
+                var endNode = int.Parse(splitet[1]);
+                var weight = int.Parse(splitet[2]);
+                var currentEdge = new Edge(startNode, endNode, weight);
+                edges.Add(currentEdge);
+
+                if (!conectedNodes.ContainsKey(startNode))
+                {
+                    conectedNodes.Add(startNode, false);
+                }
+
+                if (!conectedNodes.ContainsKey(endNode))
+                {
+                    conectedNodes.Add(endNode, false);
+                }
+
+                if (splitet.Length == 4)
+                {
+                    conectedNodes[startNode] = true;
+                    conectedNodes[endNode] = true;
+                }
+            }
+
+            edges.Sort();
+            int totalWeight = 0;
+            foreach (var edge in edges)
+            {
+                int currentWeight = edge.Weight;
+                if (totalWeight+currentWeight>budget)
+                {
+                    break;
+                }
+
+                if ((conectedNodes[edge.StartNode]^conectedNodes[edge.EndNode])
+                    ||(!conectedNodes[edge.StartNode]&&!conectedNodes[edge.EndNode]))
+                {
+                    totalWeight += edge.Weight;
+                    conectedNodes[edge.StartNode] = true;
+                    conectedNodes[edge.EndNode] = true;
+                    Console.WriteLine(edge);
+                }
+            }
+
+            Console.WriteLine("Budget used: " + totalWeight);
+        }
+    }
+}
