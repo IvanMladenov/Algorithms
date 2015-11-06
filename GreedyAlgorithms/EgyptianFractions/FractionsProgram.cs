ac1d7787e5c02891dc5237e826a1307ea23266cd
IvanMladenov <ismladenov@gmail.com>
2015-11-06 15:28:08 +0200

    Egyptian Franctions with two methods for calculation, but the recursive is not evective;

---
+++ GreedyAlgorithms/EgyptianFractions/FractionsProgram.cs
@@ -0,0 +1,70 @@
+﻿namespace EgyptianFractions
+{
+    using System;
+    using System.Collections.Generic;
+
+    class FractionsProgram
+    {
+        private static List<Fraction> result = new List<Fraction>();
+
+        static void Main(string[] args)
+        {
+            string[] inputParams = Console.ReadLine().Split('/');
+            if (int.Parse(inputParams[0]) >= int.Parse(inputParams[1]))
+            {
+                Console.WriteLine("Error (fraction is equal to or greater than 1)");
+                return;
+            }
+
+            Fraction target = new Fraction(int.Parse(inputParams[0]), int.Parse(inputParams[1]));
+            Fraction bigestEgyptian = new Fraction(1, 2);
+
+            CalculateFractions(target, bigestEgyptian);
+
+            Console.WriteLine("{0} = {1}", target, string.Join(" + ", result));
+        }
+
+        private static void CalculateFractions(Fraction target, Fraction fraction)
+        {
+            while (target.CompareTo(fraction) != 0)
+            {
+                if (target.CompareTo(fraction)<0)
+                {
+                    while (target.CompareTo(fraction)<0)
+                    {
+                        Fraction newFraction = new Fraction(fraction.Nominator, fraction.Denominator+1);
+                        fraction = newFraction;
+                    }
+                }
+                else
+                {
+                    result.Add(fraction);
+                    target = target - fraction;
+                }
+            }
+
+            result.Add(fraction);
+        }
+
+        //WORKS BUT STACKOVERFLOW WITH THIS RECURSIVE METHOD ON BIGEST DENOMINATORS
+        //private static void CalculateFractions(Fraction target, Fraction fraction)
+        //{
+        //    if (target.CompareTo(fraction)==0)
+        //    {
+        //        result.Add(fraction);
+        //        return;
+        //    }
+
+        //    if (target.CompareTo(fraction)<0)
+        //    {
+        //        CalculateFractions(target, new Fraction(fraction.Nominator, fraction.Denominator+1));
+        //    }
+        //    else
+        //    {
+        //        result.Add(fraction);
+        //        Fraction redused = target - fraction;
+        //        CalculateFractions(redused, fraction);
+        //    }
+        //}
+    }
+}
